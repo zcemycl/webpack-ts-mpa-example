7df90658f67c51330809eb73f8dc6ce8e39854b8
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-13 11:53:10 +0100

    update page

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,4 @@ import { View } from './index.view'
 import { Model } from './index.model'
 import { Controller } from './index.control'
 
-console.log('this is the index page...')
 const app = new Controller( new Model(), new View() );
