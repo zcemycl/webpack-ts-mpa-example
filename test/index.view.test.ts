e4b9a3f5bdb3ecb2394610721a06b76ea02430a0
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-13 11:18:42 +0100

    try

--- test/index.view.test.ts
+++ test/index.view.test.ts
@@ -1,10 +1,9 @@
-const { describe, expect, test } = require('@jest/globals')
+// const { describe, expect, test } = require('@jest/globals')
 const { JSDOM } = require('jsdom')
 const dom = new JSDOM('<!DOCTYPE html><div id="root"></div>');
 global.document = dom.window.document
 global.window = dom.window
 const { View } = require('../src/index.view')
-
 
 describe('testing view of index page', () => {
     test('find button and paragraph', () => {
