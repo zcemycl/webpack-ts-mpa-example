a723c44fe506937d3ce7d1a60f2abc87fde4471a
Yui Chun Leung <leo.leung@faculty.ai>
2022-09-13 16:21:45 +0100

    add model test for index page

--- test/index.control.test.ts
+++ test/index.control.test.ts
@@ -1,11 +1,34 @@
 import { Controller } from '../src/index.control'
-import { View } from '../src/index.view'
-import { Model } from '../src/index.model'
+import { IView, View } from '../src/index.view'
+import { IModel, Model } from '../src/index.model'
 
 // const app = new Controller( new Model(), new View() );
+let spy: jest.SpyInstance;
+beforeAll(() => {
+    spy = jest.spyOn(document, 'querySelector')
+})
 
 describe('test controller of index page', () => {
-    test('empty test', () => {
-        
+    let mockElement: any;
+    let view: IView;
+    let model: IModel;
+    let controller: any;
+    beforeAll(() => {
+        mockElement = document.createElement('div')
+        mockElement.setAttribute('id', 'root')
+        spy.mockReturnValue(mockElement)
+        jest.spyOn(View.prototype, 'getElement').mockReturnValue(mockElement)
+        view = new View()
+        model = new Model()
+        controller = new Controller(model, view)
+    })
+
+    afterAll(() => {
+        jest.restoreAllMocks();
+    })
+
+    test('test handleAddRandom', () => {
+        view.button.click();
+        expect(view.paragraph.innerHTML).not.toBe('');
     })
 })
