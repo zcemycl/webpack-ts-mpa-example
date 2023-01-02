c103af1e2b21e63bf3332eaffd23eed2751e7f5e
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-02 12:26:44 +0000

    try bootstrap

--- src/about/index.ts
+++ src/about/index.ts
@@ -1,9 +1,6 @@
 console.log('this is about page...')
 
-const app = document.querySelector('#root') as HTMLDivElement;
-const homeBtn = document.createElement('button') as HTMLButtonElement;
-homeBtn.textContent = "Home"
-homeBtn.onclick = () => {
+const homeCallback = (e: any) => {
     const suffix = "/"
     if (location.hostname !== "localhost" && location.hostname !== "127.0.0.1"){
         location.href = "/webpack-ts-mpa-example"+suffix;
@@ -11,4 +8,12 @@ homeBtn.onclick = () => {
         location.href = suffix;
     }
 }
+
+const app = document.querySelector('#root') as HTMLDivElement;
+const homeBtn = document.createElement('button') as HTMLButtonElement;
+homeBtn.textContent = "Home"
+homeBtn.onclick = homeCallback;
 app.append(homeBtn);
+
+const homeBtn_nav = document.getElementById('homebtn') as HTMLButtonElement;
+homeBtn_nav.onclick = homeCallback;
