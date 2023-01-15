f9c5e2aa47d700430a2bd607c111b0c36b215de0
zcemycl <42243073+zcemycl@users.noreply.github.com>
2023-01-15 10:17:20 +0000

    Deploying to gh-pages from @ zcemycl/webpack-ts-mpa-example@e89548ac345899ee80eaff331b44e90224617f6e 🚀

---
+++ typing-animation/index.bundle.js
@@ -0,0 +1,1 @@
+(()=>{"use strict";new class{constructor(t,s){this.model=t,this.view=s}}(new class{constructor(){this.data=[1,2]}},new class{constructor(){this.app=document.querySelector("#root")}})})();
