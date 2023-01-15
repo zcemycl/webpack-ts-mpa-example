9361ee95c4f965f846c3a44983a4bcd284ad90b0
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-15 00:48:37 +0000

    update threejs control with data-gui

--- src/pages/3dcontrol/index.control.ts
+++ src/pages/3dcontrol/index.control.ts
@@ -1,9 +1,12 @@
 import { IView } from './index.view'
 import { IModel } from './index.model'
+import * as THREE from 'three'
+import { GUI } from 'dat.gui'
 
 export interface IController {
   model: IModel
   view: IView
+  addPropertiesControl: (obj: THREE.Mesh, title: string) => void
 }
 
 export class Controller implements IController {
@@ -13,5 +16,31 @@ export class Controller implements IController {
   constructor(model: IModel, view: IView) {
     this.model = model
     this.view = view
+    this.addPropertiesControl(this.view.cube, 'Cube')
+    this.addPropertiesControl(this.view.object2, 'Green Sphere')
+  }
+
+  addPropertiesControl = (obj: THREE.Mesh, title: string) => {
+    const gui = new GUI()
+    const objFolder = gui.addFolder(title)
+    const objRotationFolder = objFolder.addFolder('Rotation')
+    objRotationFolder.add(obj.rotation, 'x', 0, Math.PI * 2)
+    objRotationFolder.add(obj.rotation, 'y', 0, Math.PI * 2)
+    objRotationFolder.add(obj.rotation, 'z', 0, Math.PI * 2)
+    objFolder.open()
+    objRotationFolder.close()
+    const objPositionFolder = objFolder.addFolder('Position')
+    objPositionFolder.add(obj.position, 'x', -10, 10, 2)
+    objPositionFolder.add(obj.position, 'y', -10, 10, 2)
+    objPositionFolder.add(obj.position, 'z', -10, 10, 2)
+    objFolder.open()
+    objPositionFolder.close()
+    const objScaleFolder = objFolder.addFolder('Scale')
+    objScaleFolder.add(obj.scale, 'x', -5, 5)
+    objScaleFolder.add(obj.scale, 'y', -5, 5)
+    objScaleFolder.add(obj.scale, 'z', -5, 5)
+    objFolder.add(obj, 'visible')
+    objFolder.close()
+    objScaleFolder.close()
   }
 }
--- src/pages/3dcontrol/index.view.ts
+++ src/pages/3dcontrol/index.view.ts
@@ -1,7 +1,6 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import Stats from 'three/examples/jsm/libs/stats.module'
-// import { GUI } from 'dat.gui'
 
 export interface IView {
   app: HTMLCanvasElement
@@ -11,6 +10,10 @@ export interface IView {
   geometry: THREE.BoxGeometry
   material: THREE.MeshBasicMaterial
   stats: Stats
+  cube: THREE.Mesh
+  object2: THREE.Mesh
+  debug: HTMLDivElement
+  controls: OrbitControls
   render: () => void
   animate: () => void
   onWindowResize: () => void
@@ -24,17 +27,33 @@ export class View implements IView {
   geometry: THREE.BoxGeometry
   material: THREE.MeshBasicMaterial
   cube: THREE.Mesh
+  object2: THREE.Mesh
+  debug: HTMLDivElement
+  controls: OrbitControls
   stats: Stats
   observer: ResizeObserver
 
   constructor() {
     this.app = document.querySelector('#root') as HTMLCanvasElement
+    this.debug = document.querySelector('#debug') as HTMLDivElement
     this.scene = new THREE.Scene()
+    const globalaxes = new THREE.AxesHelper(1)
+    globalaxes.position.set(-1, 0, 0)
+    this.scene.add(globalaxes)
     this.camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    this.camera.position.z = 2
+    this.camera.position.z = 10
     this.renderer = new THREE.WebGLRenderer({ canvas: this.app })
     this.renderer.setSize(window.innerWidth, window.innerHeight)
-    new OrbitControls(this.camera, this.renderer.domElement)
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+    this.controls.target.set(0, 0, 0)
+
+    const light1 = new THREE.PointLight()
+    light1.position.set(10, 10, 10)
+    this.scene.add(light1)
+
+    const light2 = new THREE.PointLight()
+    light2.position.set(-10, 10, 10)
+    this.scene.add(light2)
 
     window.addEventListener('resize', this.onWindowResize, true)
 
@@ -45,10 +64,16 @@ export class View implements IView {
     })
 
     this.cube = new THREE.Mesh(this.geometry, this.material)
+    this.cube.add(new THREE.AxesHelper(1))
     this.scene.add(this.cube)
 
     this.stats = Stats()
     document.body.appendChild(this.stats.dom)
+
+    this.object2 = new THREE.Mesh(new THREE.SphereGeometry(), new THREE.MeshPhongMaterial({ color: 0x00ff00 }))
+    this.object2.position.set(2, 0, 0)
+    this.cube.add(this.object2)
+    this.object2.add(new THREE.AxesHelper(4))
 
     this.animate()
   }
@@ -66,13 +91,22 @@ export class View implements IView {
 
   animate() {
     requestAnimationFrame(this.animate.bind(this))
-
+    this.controls.update()
     //this.stats.begin()
-    this.cube.rotation.x += 0.01
-    this.cube.rotation.y += 0.01
+    // this.cube.rotation.x += 0.01
+    // this.cube.rotation.y += 0.01
     //this.stats.end()
 
     this.render()
+
+    const cubePos = new THREE.Vector3()
+    const obj2Pos = new THREE.Vector3()
+    this.cube.getWorldPosition(cubePos)
+    this.object2.getWorldPosition(obj2Pos)
+    this.debug.innerText = `Cube\n local (${this.cube.position.toArray()})
+      global (${cubePos.toArray()})
+      Green sphere\nlocal (${this.object2.position.toArray()})
+      global (${obj2Pos.toArray()})`
 
     this.stats.update()
   }
