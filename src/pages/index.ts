396d1bba78c34f6dcf72f76977c4252570af7bae
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-17 16:46:31 +0000

    try eventlistener load

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,4 +1,13 @@
 import { View } from './index.view'
 import { Model } from './index.model'
 import { Controller } from './index.control'
-export const app = new Controller(new Model(), new View())
+
+let app = null
+
+window.addEventListener('load', () => {
+  app = new Controller(new Model(), new View())
+  const section = document.querySelector('.section') as HTMLDivElement
+  section.style.visibility = 'visible'
+})
+
+export { app }
