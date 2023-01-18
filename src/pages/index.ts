0ce1e1504f7db31b4e2a808a73b45977046dc9e7
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-18 14:09:33 +0000

    add transition to loading bar

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -7,9 +7,9 @@ let app = null
 window.addEventListener('load', () => {
   app = new Controller(new Model(), new View())
   const loadingbar = document.querySelector('.loading-bar') as HTMLDivElement
-  const section = document.querySelector('.section') as HTMLDivElement
-  section.style.visibility = 'visible'
-  loadingbar.style.display = 'none'
+  const content = document.querySelector('.content-after-loading') as HTMLDivElement
+  loadingbar.classList.toggle('off')
+  content.classList.toggle('on')
 })
 
 export { app }
