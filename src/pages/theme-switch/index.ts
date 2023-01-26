07166e88c8485121f91c9020835a66479c4a34af
Yui Chun Leung <leo.leung@faculty.ai>
2023-01-26 00:13:52 +0000

    updatetheme switch example

--- src/pages/theme-switch/index.ts
+++ src/pages/theme-switch/index.ts
@@ -1,4 +1,6 @@
 import { View } from './index.view'
 import { Model } from './index.model'
 import { Controller } from './index.control'
-export const app = new Controller(new Model(), new View())
+
+const app = new Controller(new Model(), new View())
+export { app }
--- src/pages/theme-switch/index.view.ts
+++ src/pages/theme-switch/index.view.ts
@@ -1,11 +1,46 @@
 export interface IView {
   app: HTMLElement
+  colorThemes: NodeListOf<HTMLInputElement>
+  storeTheme: (theme: string) => void
+  retrieveTheme: () => void
+  setTheme: (theme: string) => void
 }
 
 export class View implements IView {
   app: HTMLElement
+  colorThemes: NodeListOf<HTMLInputElement>
 
   constructor() {
     this.app = document.querySelector('#root') as HTMLElement
+    this.colorThemes = document.querySelectorAll("[name='theme']") as NodeListOf<HTMLInputElement>
+
+    this.colorThemes.forEach(themeOption => {
+      themeOption.addEventListener('click', () => {
+        this.storeTheme(themeOption.id)
+      })
+    })
+
+    this.retrieveTheme()
+  }
+
+  storeTheme = (theme: string) => {
+    console.log(theme)
+    localStorage.setItem('theme', theme)
+    this.setTheme(theme as string)
+  }
+
+  retrieveTheme = () => {
+    let activeTheme = localStorage.getItem('theme')
+    console.log(activeTheme)
+    if (activeTheme === null) activeTheme = 'light'
+    this.colorThemes.forEach(themeOption => {
+      if (themeOption.id === activeTheme) themeOption.checked = true
+      else themeOption.checked = false
+    })
+    this.setTheme(activeTheme as string)
+  }
+
+  setTheme = (theme: string) => {
+    document.documentElement.className = theme
   }
 }
